78384871b744c2d8d19fb9f7ab1f69fc958fcec8
thehunk1206 <khantauhid313@gmail.com>
2021-07-03 12:22:03 +0530

    Saving serial data as String in CSV file

--- Audio Dataset sampler/create_dataset.py
+++ Audio Dataset sampler/create_dataset.py
@@ -4,7 +4,6 @@ Python script to read data from serial port and write to numpy array
 This script is use to create Audio FFT dataset with arduino nano ble sense.
 
 TODO: Automatically detect arduino port
-TODO: Handle SerialException wile reading data. In case the device disconnect while reading, the numpy should save
 '''
 
 import serial
@@ -21,12 +20,24 @@ import pandas as pd
 SERIAL_TIMEOUT = 4
 DATASET_PATH = "Dataset/"
 
+'''
+Writing captured data into csv using pandas
+'''
+AUDIO_FFT_DATA_DICT = {
+    "audio_spectrum_vec": [],
+    "target": []
+}
+
 DESCRIPTION = """A python Script to create Audio FFT dataset using Arduino nana BLE sense.
                 Capture the audio from on board Microphone of Arduino nano BLE sense, convert
                 the raw audio from time domain to frequency domain using arduinoFFT"""
 
 
 def init_argparser() -> argparse.Namespace:
+    '''
+    Initialize argparser to take arguments from command line
+    '''
+
     parser = argparse.ArgumentParser(
         description=DESCRIPTION,
     )
@@ -67,55 +78,67 @@ def init_argparser() -> argparse.Namespace:
 
 
 def init_serial_port(port: str, baudrate: int = 9600):
+    '''
+    initialize serial port using pyserial to communicate with arduino.
+    Check if the port is correct or not. If not the programs exits.
+
+    args:
+        port: str
+            The port at which your arduino is connected
+            NOTE: For windows check your port in your device manager
+        baudrate: int = 9600
+            The speed at which communication is going to happen
+
+    '''
     global ser
     try:
         ser = serial.Serial(port, baudrate=baudrate, timeout=SERIAL_TIMEOUT)
         print(
             f"[info] Serial port opened \nPORT: {port}\nBaud rate: {baudrate}\n\n")
     except SerialException as e:
-        print(e.strerror)
+        print("[Error] "+e.strerror)
         sys.exit()
 
 
-def save_as_numpy_array(arr: np.ndarray, filename: str):
+# ===========util func==========================
+def clear_audio_dict():
     '''
-    saves the numpy array in .npy binary file
+    clear the dataframe dict before writing new captured data 
+    to avoid duplicates and repetition of data in csv
     '''
+    AUDIO_FFT_DATA_DICT["audio_spectrum_vec"].clear()
+    AUDIO_FFT_DATA_DICT["target"].clear()
+
+
+def write_to_csv(captured_data: str, class_label: str) -> bool:
+    clear_audio_dict()
+    AUDIO_FFT_DATA_DICT["audio_spectrum_vec"].append(captured_data)
+    AUDIO_FFT_DATA_DICT["target"].append(class_label)
+    df = pd.DataFrame(AUDIO_FFT_DATA_DICT)
+
     try:
-        np.save(f"{DATASET_PATH}{filename}", arr=arr)
-        print(f"[info] Data saved at {DATASET_PATH}{filename}.npy")
+        with open(f"{DATASET_PATH}/surrounding_audio_dataset_FFT.csv", 'a') as f:
+            df.to_csv(f, header=f.tell() == 0)
+            return True
     except:
-        print("Cannot save the numpy array")
-
-def save_as_panda_dataframe():
-
-    pass
+        return False
 
 
-def captureSerialData() -> np.ndarray:
+def captureSerialData() -> str:
     '''
     This functions does the following
     1.Read data from serial port.
     2.Decode serial data as Ascii.
-    3.Create list of string data points by split() with Deliminator as ','.
-    4.Convert to numpy array and return as float numpy array
+    3.return the serial data as string
     '''
 
     audio_data_str = ser.readline()
     audio_data_str = audio_data_str.decode('Ascii')
     audio_data_str = audio_data_str[1:len(audio_data_str)-6]
-    audio_data_str = audio_data_str.split(",")
-    audio_data_np = np.array([audio_data_str])
-    audio_data_np = audio_data_np.astype(np.float)
-
-    return audio_data_np
+    return audio_data_str
 
 
-def createDataset(
-    class_label: str,
-    number_of_data_instance: int = 100,
-    dataset: list = []
-):
+def createDataset(class_label: str, number_of_data_instance: int = 100, dataset: list = []):
     '''
     Creates numpy dataset by appending each captured audio FFT data in python list.
     Converting that python list to numpy array.
@@ -129,6 +152,7 @@ def createDataset(
         dataset : list = []
             An epmty python list where data is appended
     '''
+
     print(
         f"=============Capturing Audio FFT data=========\n \
         [info] NUMBER OF SAMPLE: {number_of_data_instance}\n \
@@ -138,12 +162,20 @@ def createDataset(
         ser.write(b'1')
         np_audio_data = captureSerialData()
         print(
-            f"[info]Capttured Sample no: {i+1} \nData shape {np_audio_data.shape}\n")
-        dataset.append(np_audio_data)
+            f"[info]Capttured Sample no: {i+1} \nData shape {len(np_audio_data)}")
+
+        has_wrtitten_to_csv = write_to_csv(
+            captured_data=np_audio_data, class_label=class_label)
+
+        if has_wrtitten_to_csv:
+            print(
+                f"[info] Captured audio FFT written to csv => {DATASET_PATH}/surrounding_audio_dataset_FFT.csv\n")
+        else:
+            print("[Error] Something went wrong while writting data to csv file")
+            sys.exit()
+
         time.sleep(0.5)
     ser.close()
-    dataset_np = np.array(dataset)
-    save_as_numpy_array(dataset_np, class_label)
 
 
 if __name__ == "__main__":
@@ -154,9 +186,10 @@ if __name__ == "__main__":
     if not os.path.exists(DATASET_PATH):
         os.mkdir(DATASET_PATH)
 
-    #initialize serial communication
+    # initialize serial communication
     init_serial_port(port=args.serialport, baudrate=args.baudrate)
 
+    # start capturing Audio FFT(Fast fourier transform) data
     createDataset(
         class_label=args.label,
         number_of_data_instance=args.dsize
