fa6f3ed3b32ed07d982d6b89a8ce14501bb7f3fe
thehunk1206 <khantauhid313@gmail.com>
2021-07-16 12:48:22 +0530

    Creating dataset with audacity

---
+++ Audio_dataset/create_audio_dataset.py
@@ -0,0 +1,30 @@
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+
+import os
+
+BASEDIR = "FullAudio/"
+CHUNK_SIZE = 1000  # chunk size in ms
+SAMPLE_RATE = 16000
+
+
+def make_chunks_of_audio(path_to_wav: str):
+    audio = AudioSegment.from_file(file=path_to_wav, format='wav')
+    audio.set_frame_rate(SAMPLE_RATE)
+    audio_chunks = make_chunks(audio, chunk_length=CHUNK_SIZE)
+    basefilename = (os.path.split(path_to_wav)[-1]). split('.')[0]
+
+    if not os.path.exists(basefilename.upper()):
+        print(f"Creating {basefilename.upper()} folder")
+        os.mkdir(basefilename.upper())
+
+    for i, chunk in enumerate(audio_chunks):
+        new_chunkname = f"{basefilename}_{i}.wav"
+        print(f"Exporting {basefilename.upper()}/{new_chunkname}")
+        chunk.export(f"{basefilename.upper()}/{new_chunkname}", format='wav')
+
+if __name__ == "__main__":
+    all_audio_files = os.listdir(BASEDIR)
+    for audio_file in all_audio_files:
+        if (".wav" in audio_file):
+            make_chunks_of_audio(f"{BASEDIR}{audio_file}")
